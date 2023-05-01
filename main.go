0e1bb025248d57d7c6d8c069284512a26f51c50b
rikutkb <iwasaki.riku.td@alumni.tsukuba.ac.jp>
2023-05-01 16:37:48 +0900

    proxyを作成し、jsonモックに対してリクエストを行うようにした

--- main.go
+++ main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+
+	"github.com/gin-gonic/gin"
+)
+
+func proxy(c *gin.Context) {
+	remote, err := url.Parse("http://172.31.0.6:8080")
+	if err != nil {
+		panic(err)
+	}
+
+	proxy := httputil.NewSingleHostReverseProxy(remote)
+	//Define the director func
+	//This is a good place to log, for example
+	proxy.Director = func(req *http.Request) {
+		req.Header = c.Request.Header
+		req.Host = remote.Host
+		req.URL.Scheme = remote.Scheme
+		req.URL.Host = remote.Host
+		req.URL.Path = c.Param("proxyPath")
+	}
+
+	proxy.ServeHTTP(c.Writer, c.Request)
+}
 
 func main() {
-	fmt.Println("Hello.World")
+
+	r := gin.Default()
+
+	//Create a catchall route
+	r.Any("/*proxyPath", proxy)
+
+	r.Run(":8000")
 }
