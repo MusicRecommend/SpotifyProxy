1d6309e110402f755b1c31348c2e68bb89d2e128
rikutkb <iwasaki.riku.td@alumni.tsukuba.ac.jp>
2023-07-29 07:43:40 +0900

    複数のORIGINを受け入れられるように修正

--- middleware.go
+++ middleware.go
@@ -2,17 +2,31 @@ package main
 
 import (
 	"os"
+	"strings"
 	"time"
 
 	"github.com/gin-contrib/cors"
 )
 
+// カンマで区切られたオリジンをパースして配列に整形
+func parseOrigins(originsENV string) []string {
+	var origins []string
+	if origins_ := strings.Split(originsENV, ","); len(origins_) != 0 {
+		for _, origin := range origins_ {
+			// 空白をTrim
+			var origin = strings.ReplaceAll(origin, " ", "")
+			if origin != "" {
+				origins = append(origins, origin)
+			}
+		}
+	}
+	return origins
+}
+
 func setCors() cors.Config {
 	return cors.Config{
 		// アクセスを許可したいアクセス元
-		AllowOrigins: []string{
-			os.Getenv("ALLOW_ORIGIN"),
-		},
+		AllowOrigins: parseOrigins(os.Getenv("ALLOW_ORIGIN")),
 		// アクセスを許可したいHTTPメソッド(以下の例だとPUTやDELETEはアクセスできません)
 		AllowMethods: []string{
 			"POST",
